ac5356e4dff559d66b3c7b58553b029674ddbc69
vignesh8458 <71544638+vignesh8458@users.noreply.github.com>
2023-07-27 18:50:45 +0530

    v1

---
+++ Automation_Edge_Basic.py
@@ -0,0 +1,11 @@
+rom selenium import webdriver
+ 
+ 
+# Driver Code
+if __name__ == '__main__':
+ 
+    # create object
+    edgeBrowser = webdriver.Edge(r"msedgedriver.exe")
+ 
+    # open browser and navigate to facebook
+    edgeBrowser.get('https://www.facebook.com')
